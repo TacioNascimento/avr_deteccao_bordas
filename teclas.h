d847042a4771af147bbb2453f146c0cb27962f9b
TacioNascimento <tacio-donascimento@hotmail.com>
2025-05-15 08:11:47 -0300

    tecla.h

---
+++ teclas.h
@@ -0,0 +1,14 @@
+#ifndef TECLAS_H
+#define TECLAS_H
+
+void teclas_init( void );
+
+char b0( void );
+char b0_rising_edge( void );
+char b0_falling_edge( void );
+
+char b1( void );
+char b1_rising_edge( void );
+char b1_falling_edge( void );
+
+#endif
